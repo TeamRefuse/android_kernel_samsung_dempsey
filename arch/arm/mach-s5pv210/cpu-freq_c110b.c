57dda3215895248c94de67a51abb9556869fe3e9
netarchy <netarchy@gmail.com>
2011-07-29 00:51:50 -0500

    Set safe default min and max freqs

--- arch/arm/mach-s5pv210/cpu-freq_c110b.c
+++ arch/arm/mach-s5pv210/cpu-freq_c110b.c
@@ -879,7 +879,11 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 		g_dvfslockval[i] = MAX_PERF_LEVEL;
 #endif
 
-	return cpufreq_frequency_table_cpuinfo(policy, freq_table);
+	cpufreq_frequency_table_cpuinfo(policy, freq_table);
+	/* define safe default min and max speeds */
+	policy->max = 1200000;
+	policy->min = 100000;
+	return 0;
 }
 
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
